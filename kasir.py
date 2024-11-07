28c85c24f4e71556df73a0d341db4c898662fcc4
= <=>
2024-11-07 13:24:47 +0700

    Perhitungan

---
+++ kasir.py
@@ -0,0 +1,11 @@
+while True:
+    print('\n\033[92m=======================================')
+    print('\033[31m      PROGRAM DISKON BARANG 12,5%      ')
+    print('\033[92m=======================================')
+
+    harga = (input('\033[33mMasukan harga barang anda: Rp. '))
+    diskon = float(12.5)/int(100)
+    hasil = int(harga)*diskon
+    hasil2 = int(harga)-int(hasil)
+
+    print(f'\n\033[38mHarga barang setelah diberikan diskon: Rp. {hasil2}')
---
+++ persen.py
@@ -0,0 +1,7 @@
+print('='*40)
+print('Pecahan To Persen')
+print('='*40)
+
+penyebut = int(input("masukan bilangan penyebut :"))
+pembilang = int(input("masukan bilangan pembilang :"))
+print (penyebut*100/pembilang,"%")
