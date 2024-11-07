5bc2dfd48b3c20fe1122f0d9c41c60fb1a42283b
= <=>
2024-11-07 13:47:47 +0700

    Pengondisian

---
+++ PilihanGanda.py
@@ -0,0 +1,12 @@
+print('siapa orang paling ganteng?')
+print('''
+      a. Andhika
+      b. meoong
+      c. siapa
+      d. lapar
+      e. kunem''')
+a=(input('masukan jawaban '))
+if a == a:
+    print ('bener')
+else:
+    print (f'salah,jawaban nya adalah{a}')
