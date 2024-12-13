416003957ff385303b492b14d7455393a544af18
= <=>
2024-12-13 15:14:12 +0700

    Geometri

---
+++ volumebola.py
@@ -0,0 +1,6 @@
+'''dibuat tanggal 13-12-2024
+oleh Andhika Noor Hidayat'''
+import math
+radius = float(input("Masukkan jari-jari bola: "))
+volume = (4/3) * math.pi * radius ** 3
+print("Volume bola:", volume)
